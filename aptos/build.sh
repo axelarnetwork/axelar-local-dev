c1724ee2af2c8998edbcf088c33d42dc21b41f3b
npty <nptytn2@gmail.com>
2022-12-14 16:12:55 +0700

    chore: fix aptos check script

--- aptos/build.sh
+++ aptos/build.sh
@@ -1,7 +1,9 @@
 # !/bin/bash
 
-if command -v aptos &>/dev/null; then
-  aptos move compile --save-metadata --package-dir aptos/modules/test
-else
-  echo "aptos not found. skip building aptos modules".
+if ! command -v aptos &> /dev/null
+then
+    echo "aptos not found. skip building modules."
+    exit
 fi
+
+aptos move compile --save-metadata --package-dir aptos/modules/test
