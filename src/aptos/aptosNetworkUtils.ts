8558e5dd3fe69de15b522d6051e2487e023016df
npty <nptytn2@gmail.com>
2023-03-20 16:53:45 +0700

    chore: skip deploy axelar modules if already done

--- src/aptos/aptosNetworkUtils.ts
+++ src/aptos/aptosNetworkUtils.ts
@@ -1,41 +1,60 @@
-import { FaucetClient } from 'aptos';
-import { evmRelayer } from '../relay';
-import { AptosNetwork } from './AptosNetwork';
+import { FaucetClient, HexString } from "aptos";
+import { evmRelayer } from "../relay";
+import { AptosNetwork } from "./AptosNetwork";
 
 export let aptosNetwork: AptosNetwork;
 
 export interface AptosNetworkConfig {
-    nodeUrl: string;
-    faucetUrl: string;
+	nodeUrl: string;
+	faucetUrl: string;
+	fundAddresses: HexString[];
 }
 
 export async function createAptosNetwork(config?: AptosNetworkConfig) {
-    const { nodeUrl, faucetUrl } = config || {
-        nodeUrl: 'http://localhost:8080',
-        faucetUrl: 'http://localhost:8081',
-    };
-    const loadingAptosNetwork = new AptosNetwork(nodeUrl);
+	const nodeUrl = config?.nodeUrl || "http://localhost:8080";
+	const faucetUrl = config?.faucetUrl || "http://localhost:8081";
+	const fundAddresses = config?.fundAddresses || [];
 
-    // fund the account with faucet
-    const faucet = new FaucetClient(nodeUrl, faucetUrl);
+	const loadingAptosNetwork = new AptosNetwork(nodeUrl);
 
-    // fund the deployer address
-    await faucet.fundAccount(loadingAptosNetwork.owner.address(), 1e10);
+	// fund the account with faucet
+	const faucet = new FaucetClient(nodeUrl, faucetUrl);
 
-    // deploy axelar framework modules
-    const tx = await loadingAptosNetwork.deployAxelarFrameworkModules();
-    console.log('Deployed Axelar Framework modules:', tx.hash);
+	// fund the deployer address
+	for (const address of [
+		...fundAddresses,
+		loadingAptosNetwork.owner.address(),
+	]) {
+		await faucet.fundAccount(address, 1e10);
+	}
 
-    // update the sequence number
-    const callContractEvents = await loadingAptosNetwork.queryContractCallEvents({ limit: 1000 });
-    loadingAptosNetwork.updateContractCallSequence(callContractEvents);
+	// Check if whether the gateway is deployed
+	const isGatewayDeployed = await loadingAptosNetwork.isGatewayDeployed();
 
-    const payGasEvents = await loadingAptosNetwork.queryPayGasContractCallEvents({ limit: 1000 });
-    loadingAptosNetwork.updatePayGasContractCallSequence(payGasEvents);
-    aptosNetwork = loadingAptosNetwork;
-    return aptosNetwork;
+	// Deploy axelar framework modules, skip if already deployed
+	if (!isGatewayDeployed) {
+		const tx = await loadingAptosNetwork
+			.deployAxelarFrameworkModules()
+			.catch((e: any) => {
+				console.error(e);
+			});
+		console.log("Deployed Axelar Framework modules:", tx.hash);
+	}
+
+	// update the sequence number
+	const callContractEvents = await loadingAptosNetwork.queryContractCallEvents({
+		limit: 1000,
+	});
+	loadingAptosNetwork.updateContractCallSequence(callContractEvents);
+
+	const payGasEvents = await loadingAptosNetwork.queryPayGasContractCallEvents({
+		limit: 1000,
+	});
+	loadingAptosNetwork.updatePayGasContractCallSequence(payGasEvents);
+	aptosNetwork = loadingAptosNetwork;
+	return aptosNetwork;
 }
 
-export async function loadAptosNetwork(nodeUrl: string = 'http://localhost:8080') {
-    aptosNetwork = new AptosNetwork(nodeUrl);
+export async function loadAptosNetwork(nodeUrl = "http://localhost:8080") {
+	aptosNetwork = new AptosNetwork(nodeUrl);
 }
