1b452a4ac22e2bffdad0b38182c12f2c82e6a5e2
npty <nptytn2@gmail.com>
2023-09-20 14:35:00 +0700

    chore: add more expect in stop testcase

--- packages/axelar-local-dev-cosmos/src/__tests__/docker.spec.ts
+++ packages/axelar-local-dev-cosmos/src/__tests__/docker.spec.ts
@@ -1,4 +1,5 @@
 import { start, stop } from "../lib/docker";
+import fetch from "node-fetch";
 
 describe("docker", () => {
   afterEach(async () => {
@@ -11,5 +12,8 @@ describe("docker", () => {
 
   it("should stop Cosmos container gracefully", async () => {
     await stop();
+    await fetch("http://localhost:1317/").catch((e) => {
+      expect(e.message).toContain("ECONNREFUSED");
+    });
   });
 });
