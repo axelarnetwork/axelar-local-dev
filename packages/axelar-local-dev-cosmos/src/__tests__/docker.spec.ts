8aaeff255f21eb3ef90d6cd87ffe479c182f602e
npty <nptytn2@gmail.com>
2023-09-20 16:16:42 +0700

    chore: add more checks in test

--- packages/axelar-local-dev-cosmos/src/__tests__/docker.spec.ts
+++ packages/axelar-local-dev-cosmos/src/__tests__/docker.spec.ts
@@ -1,13 +1,20 @@
+import { setLogger } from "@axelar-network/axelar-local-dev";
 import { start, stop } from "../lib/docker";
 import fetch from "node-fetch";
 
+setLogger(() => undefined);
 describe("docker", () => {
   afterEach(async () => {
     await stop();
   });
 
   it("should start Cosmos container successfully", async () => {
-    await start();
+    const { owner, rpcUrl, lcdUrl } = await start();
+
+    expect(owner.mnemonic).toBeDefined();
+    expect(owner.address).toBeDefined();
+    expect(rpcUrl).toBeDefined();
+    expect(lcdUrl).toBeDefined();
   });
 
   it("should stop Cosmos container gracefully", async () => {
