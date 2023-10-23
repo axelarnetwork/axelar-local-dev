dd0681e5cade262ff1fa300c06b7306f7f7be816
npty <nptytn2@gmail.com>
2023-10-23 14:02:49 +0700

    chore: allow denom to be specified

--- packages/axelar-local-dev-cosmos/docker/private/bin/init_and_add_keys.sh
+++ packages/axelar-local-dev-cosmos/docker/private/bin/init_and_add_keys.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DENOM=${DENOM:-ustake}
+
 HOME=/root/private/.${CHAIN_NAME}
 
 # Removing the existing .simapp directory to start with a clean slate
@@ -7,6 +9,9 @@ rm -rf ${HOME}
 
 # Initializing a new blockchain with identifier ${CHAIN_NAME} in the specified home directory
 wasmd init test-chain --chain-id ${CHAIN_NAME} --home ${HOME} > /dev/null 2>&1 && echo "Initialized new blockchain with chain ID ${CHAIN_NAME}"
+
+# staking/governance token is hardcoded in config, change this
+sed -i "s/\"stake\"/\"$DENOM\"/" "$HOME"/config/genesis.json && echo "Updated staking token to $DENOM"
 
 # Copying the config files
 cp /root/config/*.toml ${HOME}/config/
@@ -18,20 +23,20 @@ echo ${mnemonic} | tr -d "\n" > ${HOME}/mnemonic.txt
 echo "Added new key 'owner'"
 
 # Adding a new genesis account named 'owner' with an initial balance of 100000000stake in the blockchain
-wasmd genesis add-genesis-account owner 100000000stake \
-  --home ${HOME} \
-  --keyring-backend test > /dev/null 2>&1 && echo "Added 'owner' to genesis account"
+wasmd genesis add-genesis-account owner 100000000${DENOM} \
+--home ${HOME} \
+--keyring-backend test > /dev/null 2>&1 && echo "Added 'owner' to genesis account"
 
-# Generating a new genesis transaction for 'owner' delegating 70000000stake in the blockchain with the specified chain-id
-wasmd genesis gentx owner 70000000stake \
-  --home ${HOME} \
-  --keyring-backend test \
-  --chain-id ${CHAIN_NAME} > /dev/null 2>&1 && echo "Generated genesis transaction for 'owner'"
+# Generating a new genesis transaction for 'owner' delegating 70000000${DENOM} in the blockchain with the specified chain-id
+wasmd genesis gentx owner 70000000${DENOM} \
+--home ${HOME} \
+--keyring-backend test \
+--chain-id ${CHAIN_NAME} > /dev/null 2>&1 && echo "Generated genesis transaction for 'owner'"
 
 # Collecting all genesis transactions to form the genesis block
 wasmd genesis collect-gentxs \
-    --home ${HOME} > /dev/null 2>&1 && echo "Collected genesis transactions"
+--home ${HOME} > /dev/null 2>&1 && echo "Collected genesis transactions"
 
 
 # Starting the blockchain node with the specified home directory
-wasmd start --home ${HOME}
+wasmd start --home ${HOME} --minimum-gas-prices 0${DENOM}
