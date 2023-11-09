0d641afd83170e367d0e2ed53d3225b97a4d4100
npty <nptytn2@gmail.com>
2023-11-09 12:16:35 +0700

    chore: add start script

--- packages/axelar-local-dev-cosmos/src/index.ts
+++ packages/axelar-local-dev-cosmos/src/index.ts
@@ -3,3 +3,5 @@ export * from "./listeners";
 export * from "./services";
 export * from "./docker";
 export * from "./config";
+export * from "./utils";
+export * from "./start";
---
+++ packages/axelar-local-dev-cosmos/src/start.ts
@@ -0,0 +1,7 @@
+import { AxelarRelayerService } from "./services";
+import { defaultAxelarConfig } from './'
+
+export const startRelayer = () => [
+  const relayer = await AxelarRelayerService.create(defaultAxelarConfig);
+
+]
