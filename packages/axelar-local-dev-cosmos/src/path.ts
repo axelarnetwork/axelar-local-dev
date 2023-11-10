fc4f3544553226fae40f331b01db6866c2856a9e
npty <nptytn2@gmail.com>
2023-11-10 16:01:46 +0700

    chore: add Path to provide consistent path values

---
+++ packages/axelar-local-dev-cosmos/src/path.ts
@@ -0,0 +1,8 @@
+import path from "path";
+import { CosmosChain } from "./types";
+
+export const Path = {
+  base: path.join(__dirname, ".."),
+  docker: (chain: CosmosChain) => path.join(__dirname, "../docker", chain),
+  info: path.join(__dirname, "../info"),
+};
