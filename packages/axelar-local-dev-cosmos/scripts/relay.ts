630a7056b2b1f4d7e1c36f830d1cc84255dd9864
rabi-siddique <rabisiddique27@gmail.com>
2025-03-11 14:55:26 +0500

    initial implementation to send message to Agoric

--- packages/axelar-local-dev-cosmos/scripts/relay.ts
+++ packages/axelar-local-dev-cosmos/scripts/relay.ts
@@ -1,3 +1,10 @@
-import { relayDataToEth } from "../src/relayToEth";
+import { relayDataToEth } from '../src/relayToEth';
+import { relayDataToAgoric } from '../src/relayToAgoric';
 
-relayDataToEth();
+if (process.env.TO_AGORIC) {
+  console.log('calling relayDataToAgoric');
+  relayDataToAgoric();
+} else {
+  console.log('calling relayDataToEth');
+  relayDataToEth();
+}
---
+++ packages/axelar-local-dev-cosmos/src/relayToAgoric.ts
@@ -0,0 +1,51 @@
+import { defaultAxelarChainInfo, AxelarRelayerService } from './index';
+import {
+  evmRelayer,
+  createNetwork,
+  deployContract,
+  relay,
+  RelayerType,
+} from '@axelar-network/axelar-local-dev';
+
+export const relayDataToAgoric = async () => {
+  const axelarRelayer = await AxelarRelayerService.create(
+    defaultAxelarChainInfo
+  );
+  const ibcRelayer = axelarRelayer.ibcRelayer;
+  const ethereumNetwork = await createNetwork({ name: 'Ethereum' });
+  evmRelayer.setRelayer(RelayerType.Agoric, axelarRelayer);
+  console.log('IBC RELAYER:', JSON.stringify(ibcRelayer.srcChannelId));
+
+  // Deploy Smart Contract on the EVM (Ethereum Virtual Machine)
+  console.log('Deploying SendReceive Contract...');
+  const SendReceive = require('../artifacts/src/__tests__/contracts/SendReceive.sol/SendReceive.json');
+  const ethereumContract = await deployContract(
+    ethereumNetwork.userWallets[0],
+    SendReceive,
+    [
+      ethereumNetwork.gateway.address,
+      ethereumNetwork.gasService.address,
+      'Ethereum',
+    ]
+  );
+  console.log('Contract Deployment Successful...');
+
+  // Send a message from Ethereum Chain to Wasm Chain
+  console.log('Sending Message to Agoric...');
+  const value = BigInt(0.001 * 10 ** 18);
+  const ethereumTransaction = await ethereumContract.send(
+    'agoric',
+    'agoric15fyl0hn0armakpfs07axfrxghcc9gz695vlan5an5q6h60fw6xysjyzsff',
+    'Hi Agoric!',
+    {
+      value,
+    }
+  );
+  console.log('Ethereum Chain Transaction Hash:', ethereumTransaction.hash);
+
+  // Relay messages between Ethereum and Agoric chains
+  await relay({
+    agoric: axelarRelayer,
+    evm: evmRelayer,
+  });
+};
