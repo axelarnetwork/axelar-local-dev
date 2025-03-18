11bf34152ccf487773f0b50d76cd0ad0cbf373c1
Fraz Arshad <fraz.arshad12@gmail.com>
2025-03-18 19:34:51 +0500

    test

--- packages/axelar-local-dev-cosmos/src/__tests__/contracts/Factory.sol
+++ packages/axelar-local-dev-cosmos/src/__tests__/contracts/Factory.sol
@@ -4,8 +4,10 @@ pragma solidity  ^0.8.0;
 
 import { AxelarExecutable } from "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
 import { AxelarExecutableWithToken } from '@updated-axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutableWithToken.sol';
+import {IAxelarGasService} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
 import { StakingContract } from 'src/__tests__/contracts/StakingContract.sol';
 import { IERC20 } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol';
+import {StringToAddress, AddressToString} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/libs/AddressString.sol";
 
 contract Wallet is AxelarExecutableWithToken {
     string owner;
@@ -71,35 +73,45 @@ contract Wallet is AxelarExecutableWithToken {
 }
 
 contract Factory is AxelarExecutable {
+    using StringToAddress for string;
+    using AddressToString for address;
+
     address _gateway;
+    IAxelarGasService public immutable gasService;
+    string public chainName; // name of the chain this contract is deployed to
 
     struct Message {
-        string sender;
+        address sender;
     }
 
     Message public storedMessage; // message received from _execute
 
     constructor(
-        address gateway_
+        address gateway_,
+        address gasReceiver_,
+        string memory chainName_
     ) AxelarExecutable(gateway_) {
+        gasService = IAxelarGasService(gasReceiver_);
         _gateway = gateway_;
+        chainName = chainName_;
     }
 
-    function createVendor(string memory owner) public {
+    function createVendor(string memory owner) public returns (address) {
         address newVendorAddress = address(new Wallet(_gateway, owner));
         string memory newVendor = toAsciiString(newVendorAddress);
 
-        storedMessage = Message(newVendor);
+        storedMessage = Message(newVendorAddress);
+        return newVendorAddress;
     }
 
     function _execute(
-        string calldata /*sourceChain*/,
+        string calldata sourceChain,
         string calldata sourceAddress,
         bytes calldata payload
     ) internal override {
         // storedMessage = Message(sender, message);
-
-        createVendor(sourceAddress);
+        address vendorAddress = createVendor(sourceAddress);
+        _send(sourceChain, sourceAddress, vendorAddress);
     }
 
     function toAsciiString(address x) internal pure returns (string memory) {
@@ -117,5 +129,68 @@ contract Factory is AxelarExecutable {
     function char(bytes1 b) internal pure returns (bytes1 c) {
         if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
         else return bytes1(uint8(b) + 0x57);
+    }
+
+
+    function _send(
+        string calldata destinationChain,
+        string calldata destinationAddress,
+        address message
+    ) internal {
+        // 1. Generate GMP payload
+        bytes memory executeMsgPayload = abi.encode(
+            message
+        );
+        bytes memory payload =  abi.encodePacked(bytes4(0x00000000), executeMsgPayload);
+
+        // 2. Pay for gas
+        gasService.payNativeGasForContractCall{value: msg.value}(
+            address(this),
+            destinationChain,
+            "agoric1fmajpxpsrqyncyh4xduj9jsh3fcl820jwx3ce0rv5n6g2l4ddvss56we2y",
+            payload,
+            msg.sender
+        );
+
+        // 3. Make GMP call
+        gateway.callContract(destinationChain, "agoric1fmajpxpsrqyncyh4xduj9jsh3fcl820jwx3ce0rv5n6g2l4ddvss56we2y", payload);
+    }
+
+    function _encodePayload(
+        bytes memory executeMsgPayload
+    ) internal view returns (bytes memory) {
+        // Schema
+        //   bytes4  version number (0x00000001)
+        //   bytes   ABI-encoded payload, indicating function name and arguments:
+        //     string                   CosmWasm contract method name
+        //     dynamic array of string  CosmWasm contract argument name array
+        //     dynamic array of string  argument abi type array
+        //     bytes                    abi encoded argument values
+
+        // contract call arguments for ExecuteMsg::receive_message_evm{ source_chain, source_address, payload }
+        bytes memory argValues = abi.encode(
+            chainName,
+            address(this).toString(),
+            executeMsgPayload
+        );
+
+        string[] memory argumentNameArray = new string[](3);
+        argumentNameArray[0] = "source_chain";
+        argumentNameArray[1] = "source_address";
+        argumentNameArray[2] = "payload";
+
+        string[] memory abiTypeArray = new string[](3);
+        abiTypeArray[0] = "string";
+        abiTypeArray[1] = "string";
+        abiTypeArray[2] = "bytes";
+
+        bytes memory gmpPayload;
+        gmpPayload = abi.encode(
+            "receive_message_evm",
+            argumentNameArray,
+            abiTypeArray,
+            argValues
+        );
+
     }
 }
--- packages/axelar-local-dev-cosmos/src/relayFromEth.ts
+++ packages/axelar-local-dev-cosmos/src/relayFromEth.ts
@@ -2,6 +2,8 @@ import { defaultAxelarChainInfo, AxelarRelayerService } from "./index";
 import { encode, decode } from "@metamask/abi-utils";
 import { Contract, ethers } from "ethers";
 import { encodeVersionedPayload } from "./utils";
+import { encode as b64encode, decode as b64decode } from 'js-base64';
+import { SigningStargateClient } from '@cosmjs/stargate';
 
 import {
   evmRelayer,
@@ -25,39 +27,126 @@ export const relayDataFromEth = async () => {
   const ethereumContract = await deployContract(
     ethereumNetwork.userWallets[0],
     Factory,
-    [ethereumNetwork.gateway.address]
-  );
-
-  const gatewayContract = new Contract(
-    ethereumNetwork.gateway.address,
-    AxelarGateway.abi,
-    ethereumNetwork.ownerWallet
-  );
-  const encoder = ethers.utils.defaultAbiCoder;
-  const message = encode(["string"], ["fraz"]);
-  const payloadArguments = encode(
-    ["string", "string", "bytes"],
-    ["Ethereum", "0x0830c9d8f05D1dcAE3406102420C29bBb287C199", message]
-  );
-  const cosmwasmPayload = encoder.encode(
-    ["string", "string[]", "string[]", "bytes"],
-    [
-      "receive_message_evm",
-      ["source_chain", "source_address", "payload"],
-      ["string", "string", "bytes"],
-      payloadArguments,
+    [ethereumNetwork.gateway.address,
+      ethereumNetwork.gasService.address,
+      'Ethereum',
     ]
   );
-  console.log(cosmwasmPayload);
-  const encodedPayload = encodeVersionedPayload(0, cosmwasmPayload);
-  await gatewayContract.callContract(
-    "agoric",
-    // 
-    "agoric1c9gyu460lu70rtcdp95vummd6032psmpdx7wdy",
-    Array.from(encodedPayload)
+
+  console.log('Ethereum Contract Address:', ethereumContract.address);
+
+  // const gatewayContract = new Contract(
+  //   ethereumNetwork.gateway.address,
+  //   AxelarGateway.abi,
+  //   ethereumNetwork.ownerWallet
+  // );
+  // const encoder = ethers.utils.defaultAbiCoder;
+  // const message = encode(["string"], ["fraz"]);
+  // const payloadArguments = encode(
+  //   ["string", "string", "bytes"],
+  //   ["Ethereum", "0x0830c9d8f05D1dcAE3406102420C29bBb287C199", message]
+  // );
+  // const cosmwasmPayload = encoder.encode(
+  //   ["string", "string[]", "string[]", "bytes"],
+  //   [
+  //     "receive_message_evm",
+  //     ["source_chain", "source_address", "payload"],
+  //     ["string", "string", "bytes"],
+  //     payloadArguments,
+  //   ]
+  // );
+
+  // const object = {
+  //   "fraz": "fraz",
+  // }
+  // const encodedObj = '0x' + Buffer.from(JSON.stringify(object)).toString('hex');
+  // const encodedPayload = encodeVersionedPayload(0, encodedObj);
+  // console.log(encodedPayload)
+  // await gatewayContract.callContract(
+  //   "agoric",
+  //   // 
+  //   "agoric1rwwley550k9mmk6uq6mm6z4udrg8kyuyvfszjk",
+  //   Array.from(encodedPayload)
+  // );
+
+
+  const ibcRelayer = axelarRelayer.ibcRelayer;
+
+  const IBC_DENOM_AXL_USDC =
+    'ubld';
+  // 'ibc/295548A78785A1007F232DE286149A6FF512F180AF5657780FC89C009E2C348F';
+  const AMOUNT_IN_ATOMIC_UNITS = '1000000';
+  const CHANNEL_ID = ibcRelayer.srcChannelId;
+  const DENOM = 'ubld';
+  const AXELAR_GMP_ADDRESS =
+    'axelar1dv4u5k73pzqrxlzujxg3qp8kvc3pje7jtdvu72npnt5zhq05ejcsn5qme5';
+
+  const signer = ibcRelayer.wasmClient;
+  const senderAddress = 'agoric1estsewt6jqsx77pwcxkn5ah0jqgu8rhgflwfdl';
+
+  // TODO
+  const DESTINATION_ADDRESS = ethereumContract.address;
+  const DESTINATION_CHAIN = 'Ethereum';
+
+  const payload = encode(['string', 'string'], ['agoric1estsewt6jqsx77pwcxkn5ah0jqgu8rhgflwfdl', 'Hello, world!']);
+
+  const memo = {
+    destination_chain: DESTINATION_CHAIN,
+    destination_address: DESTINATION_ADDRESS,
+    payload: [],
+    fee: {
+      amount: '8000',
+      recipient: 'axelar1dv4u5k73pzqrxlzujxg3qp8kvc3pje7jtdvu72npnt5zhq05ejcsn5qme5',
+    },
+    type: 1,
+  };
+
+  const message = [
+    {
+      typeUrl: '/ibc.applications.transfer.v1.MsgTransfer',
+      value: {
+        sender: senderAddress,
+        receiver: AXELAR_GMP_ADDRESS,
+        token: {
+          denom: IBC_DENOM_AXL_USDC,
+          amount: AMOUNT_IN_ATOMIC_UNITS,
+        },
+        timeoutTimestamp: (Math.floor(Date.now() / 1000) + 600) * 1e9,
+        sourceChannel: CHANNEL_ID,
+        sourcePort: 'transfer',
+        memo: JSON.stringify(memo),
+      },
+    },
+  ];
+
+
+  const fee = {
+    gas: '250000',
+    amount: [{ denom: DENOM, amount: '30000' }],
+  };
+
+  console.log('Preparing to send tokens...');
+  const signingClient = await SigningStargateClient.connectWithSigner(
+    'http://localhost/agoric-rpc',
+    signer.owner
   );
 
+  const response = await signingClient.signAndBroadcast(
+    senderAddress,
+    message,
+    fee
+  );
   evmRelayer.setRelayer(RelayerType.Agoric, axelarRelayer);
+
+  await relay({
+    agoric: axelarRelayer,
+  });
+  await axelarRelayer.stopListening();
+
+
+  const ethereumMessage = await ethereumContract.storedMessage();
+  console.log('Message on Ethereum Contract:', ethereumMessage);
+
   await relay({
     evm: evmRelayer,
   });
@@ -65,5 +154,6 @@ export const relayDataFromEth = async () => {
   await relay({
     agoric: axelarRelayer,
   });
+
   await axelarRelayer.stopListening();
 };
