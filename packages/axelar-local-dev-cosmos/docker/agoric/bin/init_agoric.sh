4684f20437c3664c3078bb90eb266dea8246de48
rabi-siddique <rabisiddique27@gmail.com>
2025-04-10 11:27:10 +0500

    fix(docker-compose): simplify entrypoint and command syntax to compact string format

--- packages/axelar-local-dev-cosmos/docker/agoric/bin/init_agoric.sh
+++ packages/axelar-local-dev-cosmos/docker/agoric/bin/init_agoric.sh
@@ -8,5 +8,5 @@ HOME=/root/private/.agoric
 # Removing the existing directory to start with a clean slate
 rm -rf ${HOME}/*
 mnemonic="soap hub stick bomb dish index wing shield cruel board siren force glory assault rotate busy area topple resource okay clown wedding hint unhappy"
-echo ${mnemonic} | tr -d "\n" > ${HOME}/mnemonic.txt
+echo ${mnemonic} | tr -d "\n" >${HOME}/mnemonic.txt
 echo "Added new key 'owner'"
